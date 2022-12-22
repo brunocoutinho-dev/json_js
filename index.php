0857949f92c7439d939d46674dffb05a12acd203
Bruno do nascimento coutinho <114962938+brunocoutinho-dev@users.noreply.github.com>
2022-12-22 11:39:17 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		
+		<title>Demo</title>
+		<script src="meu_arquivo_javascript.js"></script>
+	</head>
+
+	<body>
+		<a href="" id="a">Clique aqui</a>
+		<div id="texto"></div>
+	</body>
+</html>
